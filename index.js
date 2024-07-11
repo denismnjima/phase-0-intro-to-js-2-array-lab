df2382ed282ca02be6a9de196438454170c1ca9f
denismnjima <denismnjima@gmail.com>
2024-07-11 13:14:15 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+const destructivelyPrependCat = (name)=>{
+    return cats.unshift(name)
+}
+const destructivelyRemoveLastCat = ()=>{
+    return cats.pop()
+
+}
+const destructivelyRemoveFirstCat =()=>{
+    return cats.shift()
+}
+const appendCat= (name)=>{
+    return [].concat(cats,name)
+}
+const prependCat =(name)=>{
+    return [].concat(name,cats)
+}
+const removeLastCat =()=>{
+    return cats.slice(0,2)
+}
+const removeFirstCat=()=>{
+    return cats.slice(1,3)
+}
